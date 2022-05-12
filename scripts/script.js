3a6738ea74548a08cbb6acf17d2a6033bc41ded8
Petrut <ivanoiu.petrut27@yahoo.com>
2022-05-12 15:49:10 +0300

    added dark/light theme toggle functionality

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,21 @@
-const tween = KUTE.fromTo(
-  "#blob1",
-  { path: "#blob1" },
-  { path: "#blob2" },
-  { repeat: 999, duration: 3000, yoyo: true }
-).start();
+// const tween = KUTE.fromTo(
+//   "#blob1",
+//   { path: "#blob1" },
+//   { path: "#blob2" },
+//   { repeat: 999, duration: 3000, yoyo: true }
+// ).start();
 
-tween.start();
+// tween.start();
+
+const themeButton = document.getElementById("theme-btn");
+const body = document.body;
+
+themeButton.onclick = () => {
+  if (body.classList.contains("dark")) {
+    body.classList.remove("dark");
+    body.classList.add("light");
+  } else {
+    body.classList.remove("light");
+    body.classList.add("dark");
+  }
+};
