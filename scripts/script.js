8edb45725e71614adb8dd021f82340feb3e0add0
Petrut <ivanoiu.petrut27@yahoo.com>
2022-05-13 00:47:09 +0300

    styled and added photos for the work section and made the blob animation

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,9 @@
-// const tween = KUTE.fromTo(
-//   "#blob1",
-//   { path: "#blob1" },
-//   { path: "#blob2" },
-//   { repeat: 999, duration: 3000, yoyo: true }
-// ).start();
+const tween = KUTE.fromTo(
+  "#blob1",
+  { path: "#blob1" },
+  { path: "#blob2" },
+  { repeat: 999, duration: 3000, yoyo: true }
+).start();
 
 // tween.start();
 
