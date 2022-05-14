358040437518c6d43e341b02fa678838486740d1
Ivanoiu Petrut <ivanoiu.petrut27@yahoo.com>
2022-05-14 14:18:23 +0300

    added smooth scrolling

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,5 @@
+// BLOB ANIMATION
+
 const tween = KUTE.fromTo(
   "#blob1",
   { path: "#blob1" },
@@ -5,7 +7,7 @@ const tween = KUTE.fromTo(
   { repeat: 999, duration: 3000, yoyo: true }
 ).start();
 
-// tween.start();
+//  THEME TOGGLE
 
 const themeButton = document.getElementById("theme-btn");
 const body = document.body;
@@ -19,3 +21,27 @@ themeButton.onclick = () => {
     body.classList.add("dark");
   }
 };
+
+// SMOOTH SCROLLING
+
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // Scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    // Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+  });
+});
