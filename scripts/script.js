0628755aeebcd54e866b1e0ad16ee783df527bd4
Petrut <ivanoiu.petrut27@yahoo.com>
2023-03-07 08:25:19 +0200

    feat: change logo

--- scripts/script.js
+++ scripts/script.js
@@ -3,7 +3,7 @@
 const themeButton = document.getElementById("theme-btn");
 const body = document.body;
 
-themeButton.onclick = () => {
+themeButton.addEventListener("click", function () {
   if (body.classList.contains("dark")) {
     body.classList.remove("dark");
     body.classList.add("light");
@@ -11,30 +11,7 @@ themeButton.onclick = () => {
     body.classList.remove("light");
     body.classList.add("dark");
   }
-};
-
-// SMOOTH SCROLLING
-// const allLinks = document.querySelectorAll("a:link");
-
-// allLinks.forEach(function (link) {
-//   link.addEventListener("click", function (e) {
-//     e.preventDefault();
-//     const href = link.getAttribute("href");
-
-//     // Scroll back to top
-//     if (href === "#")
-//       window.scrollTo({
-//         top: 0,
-//         behavior: "smooth",
-//       });
-
-//     // Scroll to other links
-//     if (href !== "#" && href.startsWith("#")) {
-//       const sectionEl = document.querySelector(href);
-//       sectionEl.scrollIntoView({ behavior: "smooth" });
-//     }
-//   });
-// });
+});
 
 // STICKY NAVIGATION
 const sectionHeroEl = document.querySelector(".hero");
